fc6e3b60b8de92ffb409f2ff27cfdb26fec3b016
Robert E Spivack <spiv@spivtech.com>
2014-11-06 20:37:15 -0800

    Finished

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Robert E Spivack on 10/17/14.
+//  Copyright (c) 2014 Robert E. Spivack. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
